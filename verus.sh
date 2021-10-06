21228f9b6b7f16f0f0736d83aded793d621c6c78
neoraegx99 <79630561+neoraegx99@users.noreply.github.com>
2021-10-06 15:24:41 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -2,6 +2,6 @@
 
 termux-setup-storage
 
-pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+pkg install openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
 
 ./start-ubuntu.sh
