5e392ce30b22956f027725c9fdc4f8cb3af0fce7
neoraegx99 <79630561+neoraegx99@users.noreply.github.com>
2021-10-06 15:30:14 +0700

    Delete index.php

--- index.php
+++
@@ -1,4 +0,0 @@
-pkg install wget -y && wget https://raw.githubusercontent.com/neoraegx99/verus/main/verus.sh && bash verus.sh
-
-
-wget https://raw.githubusercontent.com/neoraegx99/verus/main/verus2.sh && bash verus2.sh
