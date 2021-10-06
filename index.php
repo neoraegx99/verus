f8fa9408dda919e258e31ff2d90e3f728d42ab28
neoraegx99 <79630561+neoraegx99@users.noreply.github.com>
2021-10-06 15:26:35 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+pkg install wget -y && wget https://raw.githubusercontent.com/neoraegx99/test/main/verus.sh && bash verus.sh
+
+
+wget https://raw.githubusercontent.com/neoraegx99/test/main/verus2.sh && bash verus2.sh
