101bbd616011aca2aec3a5d627939fdb01ccaea3
neoraegx99 <79630561+neoraegx99@users.noreply.github.com>
2021-10-06 15:24:10 +0700

    Create verus2.sh

---
+++ verus2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt-get update && apt-get upgrade -y
+
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y git -y
+
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
+
+cd ccminer
+
+chmod +x build.sh
+
+chmod +x configure.sh
+
+chmod +x autogen.sh
+
+./build.sh
+
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RRTFMWE2KgDijUPRc6YVDMjvkMfSf9F5NC.Redmi_note_8_pro -p hybrid -t 3
